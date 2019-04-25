40bbfb2ad168fa59466387ce5042cc3cff713d0a
Ben Sharkey <bensharkey3@gmail.com>
2019-04-25 17:54:16 +1000

    added all components together, added try and except for api calls

--- GetCurrentPrices.py
+++ GetCurrentPrices.py
@@ -4,24 +4,6 @@ import os
 from urllib.request import Request, urlopen
 import json
 
-
-# get the latest file exported from google sheets
-list_of_files = glob.glob(r'C:\Users\user\Downloads\My Balance Sheet**.xlsx')
-ssheet = max(list_of_files, key=os.path.getctime)
-
-asx1 = pd.read_excel(r'C:\Users\user\Google Drive\asx1.xlsx', parse_dates=['close_date'])
-df1 = pd.DataFrame(columns=['code', 'close_date', 'close_price'])
-AssetInfo = pd.read_excel(ssheet, sheet_name='AssetInfo')
-asx_codes = AssetInfo[AssetInfo['Exchange'] == 'ASX']['Code'].unique()
-
-for i in asx_codes:
-    temp = pd.read_json('https://www.asx.com.au/asx/1/share/{ticker}/prices?interval=daily&count=1'.format(ticker=i), orient='split')
-    temp = temp[['code', 'close_date', 'close_price']]
-    df1 = df1.append(temp, sort=False)
-
-# list of stock symbols
-symbols = ['GOOG', 'AAPL']
-
 # api key for alpha vantage
 apikey = 'PIHXDM5NYLGDIL1M'
 functstock = 'TIME_SERIES_DAILY'
@@ -29,40 +11,61 @@ functaud = 'FX_DAILY'
 from_symbol = 'AUD'
 to_symbol = 'USD'
 
-# create an empty df to append rows to
-df = pd.DataFrame(columns=['4. close'])
-
 # get aud/usd exchange rate
-url = 'https://www.alphavantage.co/query?function={}&from_symbol={}&to_symbol={}&apikey={}'.format(functaud, from_symbol, to_symbol, apikey)
-request = Request(url)
+urlxr = 'https://www.alphavantage.co/query?function={}&from_symbol={}&to_symbol={}&apikey={}'.format(functaud, from_symbol, to_symbol, apikey)
+request = Request(urlxr)
 response = urlopen(request)
 data = json.loads(response.read())
-dfasx = pd.DataFrame.from_dict(data['Time Series FX (Daily)'], orient="index")[['4. close']]
-dfasx = dfasx[dfasx.index == df1['close_date'].max()]
+try:
+    dfasx = pd.DataFrame.from_dict(data['Time Series FX (Daily)'], orient="index")[['4. close']]
+except:
+    print('FX API not working')
+dfasx = dfasx[dfasx.index == dfasx.index.max()]
 audusd = float(dfasx.iloc[0][0])
 
+# get list of us stock codes from latest file exported from google sheets
+#symbols = ['GOOG', 'AAPL']
+list_of_files = glob.glob(r'C:\Users\user\Downloads\My Balance Sheet**.xlsx')
+ssheet = max(list_of_files, key=os.path.getctime)
+AssetInfo = pd.read_excel(ssheet, sheet_name='AssetInfo')
+us_codes = AssetInfo[AssetInfo['Exchange'] == 'NYSE']['Code'].unique()
+
+# create an empty df to append rows to
+dfus = pd.DataFrame(columns=['4. close'])
+
 # add a row to df for each symbol
-for i in symbols:
+for i in us_codes:
     symbol = i
-    url = 'https://www.alphavantage.co/query?function={}&symbol={}&apikey={}'.format(functstock, symbol, apikey)
-    request = Request(url)
+    urlus = 'https://www.alphavantage.co/query?function={}&symbol={}&apikey={}'.format(functstock, symbol, apikey)
+    request = Request(urlus)
     response = urlopen(request)
     data = json.loads(response.read())
-    i = pd.DataFrame.from_dict(data['Time Series (Daily)'], orient="index")[['4. close']]
+    try:
+        i = pd.DataFrame.from_dict(data['Time Series (Daily)'], orient="index")[['4. close']]
+    except:
+        print('US stock API not working')
     i['code'] = symbol
     i = i[i.index == i.index.max()]
-    df = df.append(i, sort=False)
-    
-df.reset_index(inplace=True)
-df.rename(columns={'4. close':'close_price', 'index':'close_date'}, inplace=True)
-df['close_price'] = df['close_price'] * (1/audusd)
+    dfus = dfus.append(i, sort=False)
 
-# concat two dfs together
-dfn = pd.concat([df1, df])
+dfus.reset_index(inplace=True)
+dfus.rename(columns={'4. close':'close_price', 'index':'close_date'}, inplace=True)
+dfus['close_price'] = dfus['close_price'].astype(float)
 
+# convert us shares price into aud
+dfus['close_price'] = dfus['close_price'] * (1/audusd)
 
+# get asx codes from the latest file exported from google sheets
+list_of_files = glob.glob(r'C:\Users\user\Downloads\My Balance Sheet**.xlsx')
+ssheet = max(list_of_files, key=os.path.getctime)
+dfasx = pd.DataFrame(columns=['code', 'close_date', 'close_price'])
+AssetInfo = pd.read_excel(ssheet, sheet_name='AssetInfo')
+asx_codes = AssetInfo[AssetInfo['Exchange'] == 'ASX']['Code'].unique()
 
+for i in asx_codes:
+    temp = pd.read_json('https://www.asx.com.au/asx/1/share/{ticker}/prices?interval=daily&count=1'.format(ticker=i), orient='split')
+    temp = temp[['code', 'close_date', 'close_price']]
+    dfasx = dfasx.append(temp, sort=False)
 
-
-
-# add error checking on api calls
+# convert to datetime melbourne time
+dfasx['close_date'] = (pd.to_datetime(dfasx['close_date']) + pd.Timedelta('24:00:00')).apply(lambda x: x.date())
